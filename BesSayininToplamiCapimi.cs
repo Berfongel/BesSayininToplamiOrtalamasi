b30cf7b4c7c5e108da5321c1ab3f07deb2e2764e
Berfongel <121492984+Berfongel@users.noreply.github.com>
2023-01-04 16:44:06 +0300

    Add files via upload

---
+++ BesSayininToplamiCapimi.cs
@@ -0,0 +1,12 @@
+﻿namespace BesSayininToplamiCapimiWebApplication.Models
+{
+    public class BesSayininToplamiCapimi
+    {
+        public int Sayi1 { get; set; }
+        public int Sayi2 { get; set; }
+        public int Sayi3 { get; set; }
+        public int Sayi4 { get; set; }
+
+        public int Sayi5 { get; set; }
+    }
+}
---
+++ HomeController.cs
@@ -0,0 +1,50 @@
+﻿using BesSayininToplamiCapimiWebApplication.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace BesSayininToplamiCapimiWebApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index(BesSayininToplamiCapimi besSayininToplamiCapimi)
+        {
+            var sayi1 = besSayininToplamiCapimi.Sayi1;
+            var sayi2 = besSayininToplamiCapimi.Sayi2;
+
+            var sayi3 = besSayininToplamiCapimi.Sayi3;
+            var sayi4 = besSayininToplamiCapimi.Sayi4;
+
+            var sayi5 = besSayininToplamiCapimi.Sayi5;
+            var toplam = sayi1 + sayi2 + sayi3 + sayi4 + sayi5;
+            var ortalama = (sayi1 + sayi2 + sayi3 + sayi4 + sayi5) / 5;
+
+            var messegaToplam = string.Empty;
+            var messegaOrtalama = string.Empty;
+            messegaToplam =" Girilen sayıların toplamı : " + toplam;
+            messegaOrtalama = " Girilen sayıların ortalaması : " + ortalama;
+
+            ViewBag.MessageToplam = messegaToplam;
+            ViewBag.MessageOrtalama = messegaOrtalama;
+
+            return View("besSayininToplamiCapimi");
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
